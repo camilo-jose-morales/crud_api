e9c929dbb6aa883fa7896ef82a204d1d78403478
Camilo Jose <camilojose@aptuno.com>
2021-12-13 15:53:22 -0500

    app initial template

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,38 @@
+/**
+ * Required External Modules
+ */
 
-import cors from 'cors';
-import express from 'express';
-import * as dotenv from "dotenv";
-import mongoose from 'mongoose';
-import helmet from "helmet";
+ import * as dotenv from "dotenv";
+ import express from "express";
+ import cors from "cors";
+ import helmet from "helmet";
+ 
+ dotenv.config();
 
-dotenv.config();
+/**
+ * App Variables
+ */
 
-if (!process.env.PORT) {
-    console.log(`Error to get ports`);
+ if (!process.env.PORT) {
     process.exit(1);
-}
+ }
+ 
+ const PORT: number = parseInt(process.env.PORT as string, 10);
+ 
+ const app = express();
 
-const PORT: number = parseInt(process.env.PORT as string, 10);
+/**
+ *  App Configuration
+ */
 
-const app = express();
+ app.use(helmet());
+ app.use(cors());
+ app.use(express.json());
 
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
+/**
+ * Server Activation
+ */
 
-const server = app.listen(PORT, () => {
+ app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
-});
-
-// Send message for default URL
-app.get('/', (req, res) => res.send('Welcome to NodeJs App using TypeScript'));
+  });
